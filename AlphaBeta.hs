c9678ab2dc5de87b2a56a8a221212c041f2e1b5e
WouterJ <waldio.webdesign@gmail.com>
2016-11-07 00:48:53 +0100

    v43 Fix inverted negamax

--- AlphaBeta.hs
+++ AlphaBeta.hs
@@ -11,7 +11,7 @@ module AlphaBeta
 , inherit'
 , depthPrune
 , applyEvaluation
-, alpha_beta
+, negamax
 , bestMoves
 , dummyMove
 , etprint
@@ -101,27 +101,29 @@ preSort (pos1, game1) (pos2, game2)
           choices2 = length $ getActiveSquares game2
           isCorner p = p `elem` [(0,0),(0,2),(0,3),(0,5),(0,6),(0,8),(2,0),(2,2),(2,3),(2,5),(2,6),(2,8),(3,0),(3,2),(3,3),(3,5),(3,6),(3,8),(5,0),(5,2),(5,3),(5,5),(5,6),(5,8),(6,0),(6,2),(6,3),(6,5),(6,6),(6,8),(8,0),(8,2),(8,3),(8,5),(8,6),(8,8),(1,1),(1,4),(1,7),(4,1),(4,4),(4,7),(7,1),(7,4),(7,7)]
 
-mapmax :: (Int, Int) -> Evaluation -> [Evaluation] -> Evaluation
-mapmax _ best [v] = maximumBy (comparing score) [best, v]
-mapmax (a, b) best (v:vs)
-    | a' >= b && trace "prun" True      = v
-    | boardFinished $ getBoard $ game v = v
-    | otherwise                         = mapmax (a', b) best' vs
-    where best' = maximumBy (comparing score) [best, v]
-          a' = maximum [a, score v]
+mapmax :: Int -> Int -> (Int, [Pos]) -> [(Int, [Pos])] -> (Int, [Pos])
+mapmax _ _ best [v]    = maximumBy (comparing fst) [best, v]
+mapmax a b best (v:vs)
+    | a' >= b                           = v
+    | otherwise                         = mapmax a' b best' vs
+    where best' = maximumBy (comparing fst) [best, v]
+          a' = maximum [a, fst v]
 
-alpha_beta :: Int -> Int -> Tree Evaluation -> (Int, [Pos])
-alpha_beta _ _ (Node (score, move, _) []) = (score, [move])
-alpha_beta a b (Node (_, move, _) subs)   = (pvv, move:pvm)
+negamax :: Int -> Int -> Tree Evaluation -> (Int, [Pos])
+negamax _ _ (Node (score, move, _) []) = (score, [move])
+negamax a b (Node (_, move, _) subs)   = (pvv, move:pvm)
     where (pvv, pvm) = negaLevel (-1000, []) a b subs
           negaLevel prev_best@(score1, _) prev_a b (x:xs)
               | score1 < b = negaLevel best4 a b xs
-              where best4 = case neg $ alpha_beta (-b) (-a') x of
+              where best4 = case neg $ negamax (-b) (-a') x of
                                 value@(score2, _) | (score2 > score1) -> value
                                                   | otherwise         -> prev_best
                     a' = maximum [score1, prev_a]
           negaLevel best _ _ _ = best
           neg (score, ms) = (-score, ms)
+
+negamax' :: Tree Evaluation -> (Int, [Pos])
+negamax' = mapmax (-1000) 1000 (-1000, []) . map (negamax (-1000) 1000) . subForest
 
 {--
  - Move search
@@ -129,8 +131,7 @@ alpha_beta a b (Node (_, move, _) subs)   = (pvv, move:pvm)
 -- |The best moves calculated
 -- | Searches through deeper levels with a dummy result as head
 bestMoves :: Tree Evaluation -> [(Int, [Pos])]
-bestMoves tree = map (\i -> removeRoot $ alpha_beta (-1000) 1000 $ depthPrune i tree) (1:[2, 4..])
-    where removeRoot (!score, moves) = (score, tail moves)
+bestMoves tree = map (\i -> negamax' $ depthPrune i tree) (1:[2, 4..])
 
 dummyMove tree = (score dummy, [move dummy])
     where dummy = rootLabel $ head $ subForest tree
